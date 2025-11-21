2549a71a1af150755f68f2dc60ab5c2c7b09644b
Nirbhay <nirbhayraut18@gmail.com>
2025-11-22 00:23:16 +0530

    Updated UI and Data

---
+++ app/src/main/java/com/example/groceryapp/DataClass.kt
@@ -0,0 +1,7 @@
+package com.example.groceryapp
+
+data class DataClass(
+    val title: String,
+    val desc: String,
+    val Image: Int
+)
--- app/src/main/java/com/example/groceryapp/MainActivity.kt
+++ app/src/main/java/com/example/groceryapp/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,6 +20,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,7 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContent {
-
+            LazyColumnDemo()
         }
     }
 }
@@ -34,19 +41,29 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LazyColumnDemo() {
     val groupedItems = mapOf(  // mapOf creates an immutable Map (key → value).
-        "Fruites" to listOf("Apple", "Banana", "Cherry", "Mango", "Grapes", "WaterMelon"),
-        "Vegetables" to listOf("Carrot", "Lettuce", "Tomato", "Onion", "Potato", "Eggplant")
+        "Fruites" to listOf(
+            DataClass("Apple", "This is Apple", R.drawable.apple),
+            DataClass("Banana", "This is Banana", R.drawable.banana),
+            DataClass("Orange", "This is Orange", R.drawable.apple),
+            DataClass("Grapes", "This is Grapes", R.drawable.banana),
+            DataClass("Mango", "This is Mango", R.drawable.apple),
+        ),
+        "Vegetables" to listOf(
+            DataClass("Apple", "This is Apple", R.drawable.apple),
+            DataClass("Banana", "This is Banana", R.drawable.banana),
+            DataClass("Orange", "This is Orange", R.drawable.apple),
+            DataClass("Grapes", "This is Grapes", R.drawable.banana),
+            DataClass("Mango", "This is Mango", R.drawable.apple),
+
+        )
     )
 
-//    val myFruites = groupedItems["Fruites"]
-//    val myVegetables = groupedItems["Vegetables"]
-
     LazyColumn(modifier = Modifier.fillMaxSize()) {
-        groupedItems.forEach { (header, items) ->
+        groupedItems.forEach { (header, itemss) ->
             stickyHeader {
                 CustomHeader(header)
             }
-            items(items){
+            items(itemss){
                 data->
                 CustomCard(data)
             }
@@ -61,30 +78,54 @@ fun CustomHeader(title: String)
         text = title,
         fontSize = 32.sp,
         modifier = Modifier
-            .background(Color.Red)
+            .background(Color.Green)
             .padding(8.dp)
-            .fillMaxSize()
+            .fillMaxWidth()
     )
 }
 
 @Composable
-fun CustomCard(items: String)
+fun CustomCard(mydata: DataClass)
 {
     Card(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(32.dp)
+            .fillMaxWidth()
+            .padding(8.dp)
     ) {
-        Text(
-            text = items,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(8.dp)
-        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(mydata.Image),
+                contentDescription = mydata.title,
+                modifier = Modifier.fillMaxWidth()
+                    .height(180.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(Modifier.height(8.dp))
+
+            Text(
+                text = mydata.title,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            Spacer(Modifier.height(8.dp))
+
+            Text(
+                text = mydata.desc,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
     }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ShowApp(){
-    //LazyColumnDemo()
+    LazyColumnDemo()
+//    val item1 = DataClass("Apple", "This is Apple", R.drawable.apple)
+//    CustomCard(item1)
 }
